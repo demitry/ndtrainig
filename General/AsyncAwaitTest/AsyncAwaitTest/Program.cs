14c7ce38dddf21fb89fe04854e03976e73e2b5fa
Dmytro Poluektov <dpolu@softserveinc.com>
2018-03-29 19:04:13 +0300

    async await initial commit

---
+++ General/AsyncAwaitTest/AsyncAwaitTest/Program.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Linq;
+using System.Text;
+using System.Threading;
+using System.Threading.Tasks;
+
+//https://www.youtube.com/watch?v=6_GTdR0gBVE
+
+namespace AsyncAwaitTest
+{
+	class Program
+	{
+		static void Main(string[] args)
+		{
+			Console.WriteLine("Starting");
+
+			var worker = new Worker();
+			worker.DoWork();
+
+			while (!worker.IsComplete)
+			{
+				Console.Write(".");
+				Thread.Sleep(100);
+			}
+
+			Console.ReadKey();
+		}
+	}
+}
---
+++ General/AsyncAwaitTest/AsyncAwaitTest/Worker.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Runtime.CompilerServices;
+using System.Threading;
+
+namespace AsyncAwaitTest
+{
+	internal class Worker
+	{
+		public bool IsComplete { get; private set; }
+
+		internal void DoWork()
+		{
+			this.IsComplete = false;
+			Console.WriteLine("Doing Work");
+
+			LongOperation();
+
+			Console.WriteLine("\nWork Completed");
+
+			IsComplete = true;
+		}
+
+		private void LongOperation()
+		{
+			Console.WriteLine("Working!");
+			Thread.Sleep(2000);
+		}
+	}
+}
