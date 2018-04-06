71cf6eb088c41b109c879f96b4a5a8756f18d924
Dmytro Poluektov <dpolu@softserveinc.com>
2018-04-06 15:18:07 +0300

    ng update user list

--- ng-book/angular-hello-world/src/app/user-item/user-item.component.ts
+++ ng-book/angular-hello-world/src/app/user-item/user-item.component.ts
@@ -1,4 +1,8 @@
-import { Component, OnInit } from '@angular/core';
+import {
+  Component,
+  OnInit,
+  Input
+} from '@angular/core';
 
 @Component({
   selector: 'app-user-item',
@@ -6,9 +10,9 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./user-item.component.css']
 })
 export class UserItemComponent implements OnInit {
-  name:string; // added name property
+  @Input() name: string; // added name property
   constructor() {
-    this.name = 'Felipe';
+    // this.name = 'Felipe';
    }
 
   ngOnInit() {
