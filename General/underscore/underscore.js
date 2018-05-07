03588119a4f90121b959df68814139694d9d3145
Dmytro Poluektov <dpolu@softserveinc.com>
2018-05-07 13:19:33 +0300

    underscore js

---
+++ General/underscore/underscore.js
@@ -0,0 +1,4 @@
+var s = document.createElement('script'); 
+s.type = 'text/javascript';
+s.src = 'https://cdnjs.cloudflare.com/ajax/libs/underscore.js/1.8.3/underscore-min.js';
+document.head.appendChild(s);
