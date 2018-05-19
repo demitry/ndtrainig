eb9bb3af76130a619baca1e53ab709f95331143a
DESKTOP-FCMOFM8\Dmitry <dpoluektov@gmail.com>
2018-05-19 14:01:55 +0300

    sql comments

--- metanit/sql/StoredProcedures.sql
+++ metanit/sql/StoredProcedures.sql
@@ -1,6 +1,6 @@
 -- https://metanit.com/sql/sqlserver/11.1.php
-Хранимые процедуры
-Создание и выполнение процедур
+-- Хранимые процедуры
+-- Создание и выполнение процедур
 
 /*
 Нередко операция с данными представляет набор инструкций, которые необходимо выполнить в определенной последовательности. Например, при добавлении покупке товара необходимо внести данные в таблицу заказов. Однако перед этим надо проверить, а есть ли покупаемый товар в наличии. Возможно, при этом понадобится проверить еще ряд дополнительных условий. То есть фактически процесс покупки товара охватывает несколько действий, которые должны выполняться в определенной последовательности. И в этом случае более оптимально будет инкапсулировать все эти действия в один объект - хранимую процедуру (stored procedure).
@@ -30,14 +30,15 @@ FROM Products
 /* SQL Error (111): 'CREATE/ALTER PROCEDURE' must be the first statement in a query batch. */
 /* SQL Error (2714): There is already an object named 'ProductSummary' in the database. */
 
+/*
 Поскольку команда CREATE PROCEDURE должна вызываться в отдельном пакете, то после команды USE, которая устанавливает текущую базу данных, используется команда GO для определения нового пакета.
 
 После имени процедуры должно идти ключевое слово AS.
 
 Для отделения тела процедуры от остальной части скрипта код процедуры нередко помещается в блок BEGIN...END:
+*/
 
-
-USE productsdb;
+-- USE productsdb;
 GO
 
 CREATE PROCEDURE ProductSummary AS
@@ -56,9 +57,8 @@ DROP PROCEDURE "dbo"."ProductSummary";
 
 
 -- https://metanit.com/sql/sqlserver/11.2.php
-Параметры в процедурах
+-- Параметры в процедурах
 
-USE productsdb;
 CREATE TABLE Products
 (
     Id INT IDENTITY PRIMARY KEY,
@@ -68,8 +68,6 @@ CREATE TABLE Products
     Price MONEY NOT NULL
 );
 
-
-USE productsdb;
 GO
 CREATE PROCEDURE AddProduct
     @name NVARCHAR(20),
@@ -80,13 +78,13 @@ AS
 INSERT INTO Products(ProductName, Manufacturer, ProductCount, Price) 
 VALUES(@name, @manufacturer, @count, @price)
 
-После названия процедуры идет список входных параметров, которые определяются также как и переменные
- - название начинается с символа @, а после названия идет тип переменной. 
- И с помощью команды INSERT значения этих параметров будут передаваться в таблицу Products.
+-- После названия процедуры идет список входных параметров, которые определяются также как и переменные
+-- - название начинается с символа @, а после названия идет тип переменной. 
+--  И с помощью команды INSERT значения этих параметров будут передаваться в таблицу Products.
 
-Используем эту процедуру:
+-- Используем эту процедуру:
 
-USE productsdb;
+-- USE productsdb;
  
 DECLARE @prodName NVARCHAR(20), @company NVARCHAR(20);
 DECLARE @prodCount INT, @price MONEY;
@@ -100,5 +98,4 @@ EXEC AddProduct @prodName, @company, @prodCount, @price
 SELECT * FROM Products
 
 /* SQL Error (137): Must declare the scalar variable "@prodName". */ 
-
 
