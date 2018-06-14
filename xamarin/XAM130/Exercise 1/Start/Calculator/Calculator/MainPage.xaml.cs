a64df7d61d7842a4e407c966d6f6b4f33c9487d4
Dmytro Poluektov <dpolu@softserveinc.com>
2018-06-14 17:53:15 +0300

    130 ex2

--- xamarin/XAM130/Exercise 1/Start/Calculator/Calculator/MainPage.xaml.cs
+++ xamarin/XAM130/Exercise 1/Start/Calculator/Calculator/MainPage.xaml.cs
@@ -12,9 +12,31 @@ namespace Calculator
 	[XamlCompilation(XamlCompilationOptions.Compile)]
 	public partial class MainPage : ContentPage
 	{
+		int currentState = 1;
+		string mathOperator;
+		double firstNumber, secondNumber;
+
 		public MainPage()
 		{
 			InitializeComponent();
+			OnClear(this, null);
 		}
+
+		void OnSelectNumber(object sender, EventArgs e)
+		{
+		}
+
+		void OnSelectOperator(object sender, EventArgs e)
+		{
+		}
+
+		void OnClear(object sender, EventArgs e)
+		{
+		}
+
+		void OnCalculate(object sender, EventArgs e)
+		{
+		}
+
 	}
 }
