80aa4991e07a9eacf91e9ece13cec840de24c644
Dmytro Poluektov <dpolu@softserveinc.com>
2018-06-15 14:08:11 +0300

    130 ex3

--- xamarin/XAM130/Exercise 1/Start/Calculator/Calculator/App.cs
+++ xamarin/XAM130/Exercise 1/Start/Calculator/Calculator/App.cs
@@ -2,8 +2,13 @@
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
-
 using Xamarin.Forms;
+using Xamarin.Forms.Xaml;
+
+//As a final step, let's turn on the XAML compiler XAMLC to compile our XAML pages.
+//	1. Add the XamlCompilation attribute to App.cs
+// 	2. Pass in the option to compileXamlCompilationOptions.Compile.
+[assembly: XamlCompilation(XamlCompilationOptions.Compile)]
 
 namespace Calculator
 {
---
+++ xamarin/XAM130/Exercise 1/Start/Calculator/Calculator/SharedResources.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Xamarin.Forms;
+
+namespace Calculator
+{
+	public static class SharedResources
+	{
+		public static Color OpButtonBkColor
+		{
+			get { return Color.FromRgb(0xff, 0xa5, 0); }
+		}
+
+		public static Color NumberBgColor
+		{
+			get { return Color.White; }
+		}
+		public static Color ClearBgColor
+		{
+			get { return Color.Gray; }
+		}
+
+		public static Color OpButtonTextColor
+		{
+			get { return Color.White; }
+		}
+		public static Color NumberButtonTextColor
+		{
+			get { return Color.Black; }
+		}
+
+		public static double ButtonFontSize
+		{
+			get { return 36; }
+		}
+		public static double LabelFontSize
+		{
+			get { return 50; }
+		}
+
+	}
+}
