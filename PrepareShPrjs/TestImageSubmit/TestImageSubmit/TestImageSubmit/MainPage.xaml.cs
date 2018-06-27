54f107189277e29b8535bd3a2d5befd75d1d8983
Dmytro Poluektov <dpoluektov@gmail.com>
2018-06-27 20:33:54 +0300

    Try take photo initial

--- PrepareShPrjs/TestImageSubmit/TestImageSubmit/TestImageSubmit/MainPage.xaml.cs
+++ PrepareShPrjs/TestImageSubmit/TestImageSubmit/TestImageSubmit/MainPage.xaml.cs
@@ -1,4 +1,5 @@
-﻿using System;
+﻿using Plugin.Media;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
@@ -12,6 +13,72 @@ namespace TestImageSubmit
 		public MainPage()
 		{
 			InitializeComponent();
+		}
+
+		private async Task TakePictureButton_ClickedAsync(object sender, EventArgs e)
+		{
+			await CrossMedia.Current.Initialize();
+
+			if( !CrossMedia.Current.IsCameraAvailable ||
+				!CrossMedia.Current.IsTakePhotoSupported)
+			{
+				await DisplayAlert("No Camera", "No Camera available", "OK");
+				return;
+			}
+
+			var file = await CrossMedia.Current.TakePhotoAsync(
+				new Plugin.Media.Abstractions.StoreCameraMediaOptions
+				{
+					SaveToAlbum = true,
+					Name = "test.jpg"
+				});
+
+			if (file == null)
+				return;
+		}
+
+		private async void UploadPictureButton_Clicked(object sender, EventArgs e)
+		{
+			if(!CrossMedia.Current.IsPickPhotoSupported)
+			{
+				await DisplayAlert("No upload", "Picking a photo is not supported", "OK");
+				return;
+			}
+
+			var file = await CrossMedia.Current.PickPhotoAsync();
+
+			if(file == null)
+			{
+				return;
+			}
+
+			Image1.Source = ImageSource.FromStream(() => file.GetStream());
+		}
+
+		private async void TakePictureButton_Clicked(object sender, EventArgs e)
+		{
+			await CrossMedia.Current.Initialize();
+
+			if (!CrossMedia.Current.IsCameraAvailable ||
+				!CrossMedia.Current.IsTakePhotoSupported)
+			{
+				await DisplayAlert("No Camera", "No Camera available", "OK");
+				return;
+			}
+
+			var file = await CrossMedia.Current.TakePhotoAsync(
+				new Plugin.Media.Abstractions.StoreCameraMediaOptions
+				{
+					SaveToAlbum = true,
+					Name = "test.jpg"
+				});
+
+			if (file == null)
+			{
+				return;
+			}
+
+			Image1.Source = ImageSource.FromStream(() => file.GetStream());
 		}
 	}
 }
